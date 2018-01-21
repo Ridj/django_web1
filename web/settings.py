0521c3788f1ea2c8f94059d2e28b2e858180b04b
Ridj <katobasiro@gmail.com>
2018-01-22 00:00:34 +0600

    ver. 0.6.1.6

--- web/settings.py
+++ web/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/web1/static/')
 
 # My settings.
 LOGIN_URL = '/users/login/'
