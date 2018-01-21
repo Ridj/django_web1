f0f0ae86d5df995e2728ac76af0921ede8ae5fde
Ridj <katobasiro@gmail.com>
2018-01-21 22:15:19 +0600

    ver. 0.5.98 -debug off

--- web/settings.py
+++ web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iw^a!i^terjv_y9gq(8ln)waah-cjczl_a1^k4l#jca^#kjx+a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
