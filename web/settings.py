906470f93e23d096cb6001cc17e46b37498573f5
Ridj <katobasiro@gmail.com>
2018-01-21 20:41:30 +0600

    ver. 0.5.7 --debugMODE off in Django settings

--- web/settings.py
+++ web/settings.py
@@ -149,8 +149,10 @@ if os.getcwd() == '/app':
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+    # Allow only Heroku to host the project.
+    ALLOWED_HOSTS = ['sexy-day.herokuapp.com']
+
+    DEBUG = False
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
